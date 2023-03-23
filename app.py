886d4d4e1747f1aff93b4c91134e1712a16ab412
waielonh <waielonh@student.ubc.ca>
2023-03-22 18:34:40 -0700

    Finished App layout

--- app.py
+++ app.py
@@ -13,150 +13,235 @@ world_data = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 world_data = world_data.to_crs('EPSG:4326')
 
 meteor_world_data = gpd.sjoin(world_data, data, how='inner', predicate='contains')
-# Create default map
-def create_map(data):
-    fig = px.scatter_mapbox(data,
-                            lat='reclat',
-                            lon='reclong',
-                            size='mass',
-                            size_max=75,
-                            color='mass',
-                            mapbox_style='carto-darkmatter',
-                            center={'lat': 0, 'lon': 0},
-                            zoom=1,
-                            template='plotly_dark',
-                            color_continuous_scale=px.colors.sequential.Sunsetdark)
-    fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
-    return fig
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
 server = app.server
 
-app.layout = html.Div(
-    [
-        # This Row contains the Headings
-        dbc.Row(
-            dbc.Col(
-                html.Div(
-                    [
-                        html.H1('Meteorite Exploration Dashboard'),
-                        html.Hr(),
-                        html.P('This is an exploratory tool for meteorite craters location data')
-                    ]
-                ),
-                width='auto'
-            )
-        ),
-
-        # This row contains the rest of the dashboard
-        dbc.Row(
-            [   
-                # Input column
+app.layout = dbc.Container(
+    html.Div(
+        [
+            # This Row contains the Headings
+            dbc.Row(
                 dbc.Col(
                     html.Div(
                         [
-                            html.H3('Filter Tools'),
+                            html.H1('Meteorite Exploration Dashboard'),
                             html.Hr(),
-                            html.H4('Year range:'),
-                            dcc.Input(id='yearlow', type='number', value=860),
-                            dcc.Input(id='yearhigh', type='number', value=2013),
-                            html.Hr(),
-                            html.H4('Type of Meteor'),
-                            dcc.RadioItems(
-                                id='falltype',
-                                options=['Fell', 'Found', 'Both'],
-                                value='Both',
-                                labelStyle={'display': 'block'}
-                            ),
-                            html.Hr(),
-                            html.H4('Summary statistic'),
-                            dbc.DropdownMenu(
-                                label="Menu",
-                                children=[
-                                    dbc.DropdownMenuItem("count"),
-                                    dbc.DropdownMenuItem("mean mass"),
-                                    dbc.DropdownMenuItem("median mass"),
-                                ],
-                            ),
-                            html.Hr(),
-                            html.H4('Mass of Meteorite'),
-                            dcc.RangeSlider(id='slidermass',
-                                            min=0,
-                                            max=data['mass'].max(),
-                                            value=[0, data['mass'].max()])
+                            html.P('This is an exploratory tool for meteorite craters location data')
                         ]
                     ),
-                    width=2
-                ),
+                    width='auto'
+                )
+            ),
 
-                # Visualization column
-                dbc.Col(
-                    [
-                        # Row containing map
-                        dbc.Row(
-                            dbc.Col(
-                                html.Div(
-                                    dcc.Graph(id='map_plot', figure=create_map(data))
-                                ),
-                                width=10
-                            )
-                        ),
-
-                        # Row containing charts
-                        dbc.Row(
+            # This row contains the rest of the dashboard
+            dbc.Row(
+                [   
+                    # Input column
+                    dbc.Col(
+                        html.Div(
                             [
-                                dbc.Col(
-                                    html.Div(
-                                        dcc.Graph(id='histogram', figure={})
-                                    )
+                                html.H3('Filter Tools'),
+                                dbc.Card(
+                                    [
+                                        html.H4('Year Range', style={'padding': 10}),
+                                        dcc.RangeSlider(
+                                            id="yearrange",
+                                            min=data['year'].min(),
+                                            max=data['year'].max(),
+                                            step=1,
+                                            value=[data['year'].min(), data['year'].max()],
+                                            marks={
+                                                860: {'label': '860'},
+                                                1000: {'label': '1000'},
+                                                1250: {'label': '1250'},
+                                                1500: {'label': '1500'},
+                                                1750: {'label': '1750'},
+                                                1900: {'label': '1900'},
+                                                2013: {'label': '2013'}
+                                            },
+                                            tooltip={"placement": "bottom", "always_visible": False}
+                                        )
+                                    ]
                                 ),
-                                dbc.Col(
-                                    html.Div(
-                                        dcc.Graph(id='line', figure={})
-                                    )
+                                html.Hr(),
+                                dbc.Card(
+                                    [
+                                        html.H4('Type of Meteor', id='meteortype'),
+                                        dbc.RadioItems(
+                                            id='falltype',
+                                            options=['Fell', 'Found', 'Both'],
+                                            value='Both',
+                                            labelStyle={'display': 'block'}
+                                        )
+                                    ],
+                                    style={'padding': 10}
+                                ),
+                                html.Hr(),
+                                dbc.Card(
+                                    dbc.Switch(
+                                        id='togglesize',
+                                        label='Toggle meteor masses',
+                                        value=True
+                                    ),
+                                    style={'padding': 10}
+                                ),
+                                html.Hr(),
+                                dbc.Card(
+                                    [
+                                        html.H4('Mass of Meteorite'),
+                                        dbc.RadioItems(
+                                            id='massrange',
+                                            options=[
+                                                {'label': 'less than 10kg', 'value': 10},
+                                                {'label': 'between 10kg and 100kg', 'value': 100},
+                                                {'label': 'between 100kg and 1000kg', 'value': 1000},
+                                                {'label': 'between 1000kg and 100000kg', 'value': 100000},
+                                                {'label': 'greater than 100000kg', 'value': 100000000},
+                                                {'label': 'all', 'value': 0}
+                                            ],
+                                            value=0,
+                                            labelStyle={'display': 'block'}
+                                        )
+                                    ],
+                                    style={'padding': 10}   
                                 )
                             ]
-                        )
-                    ]
-                )
-            ]
-        )
-    ]
-)
+                        ),
+                        width=3,
+                        style={'padding': 20}
+                    ),
 
-# @app.callback(
-#     Output('map_plot', 'children'),
-#     Input('yearlow', 'value'),
-#     Input('yearhigh', 'value')
-# )
-# def update_map_plot(yearlow, yearhigh):
-#     filtered_data = data.query('year >= @yearlow and year <= yearhigh')
-#     fig = px.scatter_mapbox(filtered_data,
-#                             lat='reclat',
-#                             lon='reclong',
-#                             size='mass',
-#                             size_max=75,
-#                             color='mass',
-#                             mapbox_style='carto-darkmatter',
-#                             center={'lat': 0, 'lon': 0},
-#                             zoom=1,
-#                             template='plotly_dark',
-#                             color_continuous_scale=px.colors.sequential.Sunsetdark)
-#     fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
-#     return fig
+                    # Visualization column
+                    dbc.Col(
+                        [
+                            # Row containing map
+                            dbc.Row(
+                                dbc.Col(
+                                    html.Div(
+                                        dcc.Graph(id='map_plot', figure={})
+                                    )
+                                ),
+                            ),
+
+                            # Row containing charts
+                            dbc.Row(
+                                [
+                                    dbc.Col(
+                                        html.Div(
+                                            dbc.Card(
+                                                dcc.Graph(id='histogram', figure={}),
+                                                style={'padding': 10}
+                                            )
+                                        )
+                                    ),
+                                    dbc.Col(
+                                        html.Div(
+                                            dbc.Card(
+                                                dcc.Graph(id='line', figure={}),
+                                                style={'padding': 10}
+                                            )
+                                        )
+                                    )
+                                ],
+                                style={'padding': 10}
+                            )
+                        ],
+                        width=9
+                    )
+                ]
+            )
+        ]
+    ),
+    fluid=True
+)
+@app.callback(
+    Output('map_plot', 'figure'),
+    Input('yearrange', 'value'),
+    Input('falltype', 'value'),
+    Input('massrange', 'value'),
+    Input('togglesize', 'value')
+)
+def update_map_plot(yearrange, ftype, rmass, toggle):
+    
+    if rmass == 10:
+        massl, massh, size_max = 0, 10, 20
+    elif rmass == 100:
+        massl, massh, size_max = 10, 100, 20
+    elif rmass == 1000:
+        massl, massh, size_max = 100, 1000, 20
+    elif rmass == 100000:
+        massl, massh, size_max = 1000, 100000, 20
+    elif rmass == 100000000:
+        massl, massh, size_max = 100000, 100000000, 75
+    else:
+        massl, massh, size_max = 0, 100000000, 75
+
+    yearl, yearh = yearrange[0], yearrange[1]
+    filtered_data = meteor_world_data.query("year <= @yearh and year >= @yearl")
+    filtered_data = filtered_data.query("mass >= @massl and mass <= @massh")
+    if ftype == 'Fell':
+        filtered_data = filtered_data.query("fall == 'Fell'")
+    elif ftype == 'Found':
+        filtered_data = filtered_data.query("fall == 'Found'")
+    else:
+        pass
+    if toggle:
+        fig = px.scatter_mapbox(filtered_data,
+                                lat='reclat',
+                                lon='reclong',
+                                size='mass',
+                                size_max=size_max,
+                                color='mass',
+                                mapbox_style='carto-darkmatter',
+                                center={'lat': 0, 'lon': 0},
+                                hover_name='name_left',
+                                hover_data=['id', 'mass', 'name_right', 'year', 'recclass'],
+                                zoom=1.5,
+                                template='plotly_dark',
+                                color_continuous_scale=px.colors.sequential.Sunsetdark)
+        fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0},
+                          height=500)
+    else:
+        fig = px.scatter_mapbox(filtered_data,
+                                lat='reclat',
+                                lon='reclong',
+                                color='mass',
+                                mapbox_style='carto-darkmatter',
+                                center={'lat': 0, 'lon': 0},
+                                hover_name='name_left',
+                                hover_data=['id', 'mass', 'name_right', 'year', 'recclass'],
+                                zoom=1.5,
+                                template='plotly_dark',
+                                color_continuous_scale=px.colors.sequential.Sunsetdark)
+        fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0},
+                          height=500)
+    return fig
 
 # callback for updating histogram plot
 @app.callback(
     Output('histogram', 'figure'),
-    Input('yearlow', 'value'),
-    Input('yearhigh', 'value'),
+    Input('yearrange', 'value'),
     Input('falltype', 'value'),
-    Input('slidermass', 'value')
+    Input('massrange', 'value')
 )
-def update_histogram(yearl, yearh, ftype, rmass):
+def update_histogram(yearrange, ftype, rmass):
 
-    massl, massh = rmass[0], rmass[1]
+    if rmass == 10:
+        massl, massh = 0, 10
+    elif rmass == 100:
+        massl, massh = 10, 100
+    elif rmass == 1000:
+        massl, massh = 100, 1000
+    elif rmass == 100000:
+        massl, massh = 1000, 100000
+    elif rmass == 100000000:
+        massl, massh = 100000, 100000000
+    else:
+        massl, massh, size_max = 0, 100000000, 75
+
+    yearl, yearh = yearrange[0], yearrange[1]
     filtered_data = meteor_world_data.query("year <= @yearh and year >= @yearl")
     filtered_data = filtered_data.query("mass >= @massl and mass <= @massh")
     if ftype == 'Fell':
@@ -175,23 +260,37 @@ def update_histogram(yearl, yearh, ftype, rmass):
                   .sort_values('count', ascending=False)),
                  x='continent',
                  y='count',
-                template='plotly_dark')
+                 labels={'count': 'Number of Meteorites', 'continent': 'Continent'},
+                 template='plotly_dark')
     fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0},
                       plot_bgcolor='rgba(0,0,0,0)',
-                      paper_bgcolor='rgba(0,0,0,0)')
+                      paper_bgcolor='rgba(0,0,0,0)',
+                      height=300)
     return fig
 
 # callback for updating line plot
 @app.callback(
     Output('line', 'figure'),
-    Input('yearlow', 'value'),
-    Input('yearhigh', 'value'),
+    Input('yearrange', 'value'),
     Input('falltype', 'value'),
-    Input('slidermass', 'value')
+    Input('massrange', 'value')
 )
-def update_line(yearl, yearh, ftype, rmass):
+def update_line(yearrange, ftype, rmass):
 
-    massl, massh = rmass[0], rmass[1]
+    if rmass == 10:
+        massl, massh = 0, 10
+    elif rmass == 100:
+        massl, massh = 10, 100
+    elif rmass == 1000:
+        massl, massh = 100, 1000
+    elif rmass == 100000:
+        massl, massh = 1000, 100000
+    elif rmass == 100000000:
+        massl, massh = 100000, 100000000
+    else:
+        massl, massh, size_max = 0, 100000000, 75
+
+    yearl, yearh = yearrange[0], yearrange[1]
     filtered_data = meteor_world_data.query("year <= @yearh and year >= @yearl")
     filtered_data = filtered_data.query("mass <= @massh and mass >= @massl")
     if ftype == 'Fell':
@@ -210,10 +309,12 @@ def update_line(yearl, yearh, ftype, rmass):
                   x='year',
                   y='count',
                   color='continent',
+                  labels={'count': 'Number of Meteorites', 'year': 'Year'},
                   template='plotly_dark')
     fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0},
                       plot_bgcolor='rgba(0,0,0,0)',
-                      paper_bgcolor='rgba(0,0,0,0)')
+                      paper_bgcolor='rgba(0,0,0,0)',
+                      height=300)
     
     return fig
 
